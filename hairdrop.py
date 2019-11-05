af26ee2a9d3a965b67d19e3a39a626f01615ee37
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2019-11-05 12:04:38 +0100

    porting hairdrop to nuls 2

--- hairdrop.py
+++ hairdrop.py
@@ -3,52 +3,65 @@ import json
 import time
 import math
 import yaml
+import click
 from secp256k1 import PrivateKey
 
-from common import BASE_URL, get_utxo, prepare_contract_call_tx, broadcast, get_address, distribution_packer
+from common import contract_call_packer, broadcast, get_address
+from nuls2.api.server import get_server
 
-
-
-    
-
-
-async def main():
-    with open("config.yaml", 'r') as stream:
+async def rmain(config_file, targets_file, remark):
+    with open(config_file, 'r') as stream:
         config = yaml.safe_load(stream)
         
-    pri_key = bytes.fromhex(config['airdrop_pkey'])
+    pri_key = bytes.fromhex(config['distribution_pkey'])
+    # address = await get_address(pub_key, config['chain_id'], config['prefix'])
+    # pri_key = bytes.fromhex(config['source_pkey'])
     privkey = PrivateKey(pri_key, raw=True)
     pub_key = privkey.pubkey.serialize()
-    address = await get_address(pub_key, config['network_id'])
-    print(address)
+    address = await get_address(pub_key, config['chain_id'], config['prefix'])
+    server = get_server(config['api_server'])
+    
+    distribution_list = []    
+    content = json.load(open(targets_file, 'r'))
+    distribution_list = [
+        (item['address'], item['amount']*(10**10))
+        for item in content
+    ]
+    print(distribution_list)
 
-    addresses = []
-    for line in open('/home/jon/Documents/Aleph/snapshots/cached_unspent-testnet-20190527.json', 'r'):
-        val = json.loads(line)
-        if val['_id'].startswith('TT'):
-            unspent = val['unspent_value']
-            if isinstance(unspent, dict):
-                unspent = int(unspent['$numberLong'])
-            
-            if unspent >= 99500000:
-                addresses.append(val['_id'])
-
-    nutxo = None
+    nonce = None
 #    for address in addresses[:50]:
 #        nutxo = await nuls_packer([address],
 #                                  method='transfer', gas_limit=20000,
 #                                  gas_price=100,
 #                                  utxo=nutxo)
 #        print(address)
-    MAX_ITEMS = 120
-    for i in range(math.ceil(len(addresses) / MAX_ITEMS)):
-        nutxo = await distribution_packer(address, config['contract_address'],
-                                          addresses[MAX_ITEMS*i:MAX_ITEMS*(i+1)],
-                                          pri_key,
-                                          utxo=nutxo, remark="ALT2 Airdrop 1b",
-                                          value=100)
-        print(i, len(addresses[MAX_ITEMS*i:MAX_ITEMS*(i+1)]))
+    max_items = config.get('bulk_max_items')
+    if len(distribution_list):
+        for i in range(math.ceil(len(distribution_list) / max_items)):
+            step_items = distribution_list[max_items*i:max_items*(i+1)]
+            nash = await contract_call_packer(
+                server, config['distribution_address'], config['contract_address'],
+                'bulkTransferFrom',
+                [[config['source_address'],],
+                 [i[0] for i in step_items],
+                 [str(int(i[1])) for i in step_items]],
+                pri_key, nonce=nonce, remark=remark,
+                chain_id=config['chain_id'],
+                asset_id=config.get('asset_id', 1),
+                gas_limit=len(step_items)*30000)
+            nonce = nash[-16:]
+            await asyncio.sleep(10)
+            print("distribution stage", i, len(step_items), "items")
+            
+@click.command()
+@click.option('--config', '-c', default='config.yaml', help='Config file')
+@click.option('--targets', '-t', default='targets.json', help='Targets file')
+@click.option('--remark', '-r', default='ALEPH-BOUNTY', help='Remark')
+def main(config, targets, remark):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(rmain(config, targets, remark))
 
+if __name__ == '__main__':
+    main()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
